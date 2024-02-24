0f096aec5115ff1f98e5d78b6d56438b477fae2a
Arnav Komaragiri <komaraac@mail.uc.edu>
2024-02-24 14:38:37 -0500

    added larger logic around adding page to chunk table

--- chap-backend/chap_backend/memory.py
+++ chap-backend/chap_backend/memory.py
@@ -6,6 +6,7 @@ from typing import List, Dict
 from langchain.text_splitter import RecursiveCharacterTextSplitter, HTMLHeaderTextSplitter, Document
 
 TEXT_TYPE = Enum("TEXT_TYPE", ["PDF", "HTML"])
+CHUNK_TABLE_NAME = "PAGECHUNKS"
 
 def parse_text(text: str, text_type: TEXT_TYPE, chunk_size: int = 500, chunk_overlap: int = 30) -> List[Document]:
     docs = [text]
@@ -31,9 +32,43 @@ def connect(connection_str: str) -> psycopg2.extensions.connection:
 def disconnect(connection: psycopg2.extensions.connection):
     connection.close()
 
-def create_table(cursor: psycopg2.extensions.cursor, table_name: str):
+def create_table(connection: psycopg2.extensions.connection, cursor: psycopg2.extensions.cursor, table_name: str):
     cursor.execute(f"CREATE TABLE {table_name} (conv_id text, chunk_id text, doc text);")
+    connection.commit()
 
-def push_data(cursor: psycopg2.extensions.cursor, table_name: str, conv_id: str, chunks: List[Document]):
+# this should be considered a war crime
+def table_exists(connection: psycopg2.extensions.connection, cursor: psycopg2.extensions.cursor, table_name: str) -> bool:
+    try:
+        cursor.execute(f"SELECT * FROM {table_name};")
+    except Exception as e:
+        cursor.execute("rollback;")
+        return True
+    return False
+
+def push_data(connection: psycopg2.extensions.connection, cursor: psycopg2.extensions.cursor, 
+              table_name: str, conv_id: str, chunks: List[Document]):
     for chunk in chunks:
         cursor.execute(f"INSERT INTO {table_name} (conv_id, chunk_id, doc) VALUES ('{conv_id}', '{chunk['uuid']}', '{chunk['doc'].page_content}');")
+    connection.commit()
+
+def load_page(text: str, text_type: TEXT_TYPE, connection_str: str, **kwargs) -> str:
+    conn = connect(connection_str)
+    conv_id = None
+    try:
+        cur = conn.cursor()
+        if not table_exists(conn, cur, CHUNK_TABLE_NAME):
+            # this is also garbage
+            try:
+                create_table(conn, cur, CHUNK_TABLE_NAME)
+            except Exception as e:
+                cur.execute("rollback;")
+                pass
+        chunks = parse_text(text, text_type=text_type, **kwargs)
+        conv_id = uuid.uuid4()
+        push_data(conn, cur, CHUNK_TABLE_NAME, conv_id, chunks)
+        cur.close()
+    except Exception as e:
+        print(e)
+        cur.execute("rollback;")
+    disconnect(conn)
+    return conv_id
