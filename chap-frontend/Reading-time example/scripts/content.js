caf317869e7949953103611b28578bf1603c3a49
Jack <jack.mckain01@gmail.com>
2024-02-24 13:24:29 -0500

    Both working chrome extensions

--- chap-frontend/Reading-time example/scripts/content.js
+++ chap-frontend/Reading-time example/scripts/content.js
@@ -17,16 +17,6 @@ const article = document.querySelector('article');
 // `document.querySelector` may return null if the selector doesn't match anything.
 if (article) {
   const text = article.textContent;
-  /**
-   * Regular expression to find all "words" in a string.
-   *
-   * Here, a "word" is a sequence of one or more non-whitespace characters in a row. We don't use the
-   * regular expression character class "\w" to match against "word characters" because it only
-   * matches against the Latin alphabet. Instead, we match against any sequence of characters that
-   * *are not* a whitespace characters. See the below link for more information.
-   *
-   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-   */
   const wordMatchRegExp = /[^\s]+/g;
   const words = text.matchAll(wordMatchRegExp);
   // matchAll returns an iterator, convert to array to get word count
@@ -45,3 +35,5 @@ if (article) {
   // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
   (date ?? heading).insertAdjacentElement('afterend', badge);
 }
+
+
