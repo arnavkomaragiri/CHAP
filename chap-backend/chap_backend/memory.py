0b2653b7cb9939bd028509dd6bd6d55b0b38c050
Arnav Komaragiri <komaraac@mail.uc.edu>
2024-02-24 14:02:29 -0500

    added postgres connections, basic database interaction methods

--- chap-backend/chap_backend/memory.py
+++ chap-backend/chap_backend/memory.py
@@ -1,5 +1,8 @@
+import psycopg2
+import uuid
+
 from enum import Enum
-from typing import List
+from typing import List, Dict
 from langchain.text_splitter import RecursiveCharacterTextSplitter, HTMLHeaderTextSplitter, Document
 
 TEXT_TYPE = Enum("TEXT_TYPE", ["PDF", "HTML"])
@@ -19,4 +22,18 @@ def parse_text(text: str, text_type: TEXT_TYPE, chunk_size: int = 500, chunk_ove
     
     char_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     docs = char_splitter.split_documents(docs)
-    return docs
+    chunks = [{'uuid': uuid.uuid4(), 'doc': doc} for doc in docs]
+    return chunks
+
+def connect(connection_str: str) -> psycopg2.extensions.connection:
+    return psycopg2.connect(connection_str)
+
+def disconnect(connection: psycopg2.extensions.connection):
+    connection.close()
+
+def create_table(cursor: psycopg2.extensions.cursor, table_name: str):
+    cursor.execute(f"CREATE TABLE {table_name} (conv_id text, chunk_id text, doc text);")
+
+def push_data(cursor: psycopg2.extensions.cursor, table_name: str, conv_id: str, chunks: List[Document]):
+    for chunk in chunks:
+        cursor.execute(f"INSERT INTO {table_name} (conv_id, chunk_id, doc) VALUES ('{conv_id}', '{chunk['uuid']}', '{chunk['doc'].page_content}');")
