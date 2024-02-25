c7831e86a531bf5d7fb6b4581b41b5d87367a5bc
Arnav Komaragiri <komaraac@mail.uc.edu>
2024-02-24 19:19:55 -0500

    UNSTABLE: added RAG integration into LLM inference

--- chap-backend/llm_handler.py
+++ chap-backend/llm_handler.py
@@ -6,6 +6,10 @@ import os
 from openai import OpenAI
 import ollama
 
+from chap_backend.anon import init_mask_pipeline, anonymize
+from chap_backend.memory import *
+from chap_backend.prompts import *
+
 class Item(BaseModel):
     service: str
     prompt: str
@@ -15,9 +19,22 @@ app = FastAPI()
 
 os.environ['OPENAI_API_KEY'] = ""  # Your OpenAI API key
 
+data_set = {}
+num_retrieved = 3
+
 @app.post("/")
 async def api_endpoint(item: Item):
-    message = item.prompt + " " + item.data
+    conn = connect(os.getenv('POSTGRES_CONNECTION_STR'))
+    if item.data not in data_set:
+        conv_id = load_page(conn, item.data, TEXT_TYPE.PDF)     
+        data_set[item.data] = conv_id
+
+    results = vector_search(conn, data_set[item.data], item.prompt, k=num_retrieved)
+    context = ""
+    for i, r in enumerate(results):
+        context += f"Document {i + 1}: {r['doc']}\n"
+    
+    message = system_prompt + "\nContext:\n" + context + f"User Question: {item.prompt}"
     if item.service == 'openai':
         print("openai picked")
         client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
