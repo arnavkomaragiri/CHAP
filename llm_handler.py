d72c61470fa1617361adabef686a7323533a02e4
Andrew Gerstenslager <andrewgerstenslager@gmail.com>
2024-02-24 21:26:40 -0500

    modified message to be prompt and data

--- llm_handler.py
+++ llm_handler.py
@@ -1,5 +1,4 @@
 #python -m uvicorn llm_handler:app --reload
-
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import requests
@@ -9,40 +8,41 @@ import ollama
 
 class Item(BaseModel):
     service: str
-    message: str
+    prompt: str
+    data: str
 
 app = FastAPI()
 
-os.environ['OPENAI_API_KEY'] = "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"  # Your OpenAI API key
+os.environ['OPENAI_API_KEY'] = ""  # Your OpenAI API key
 
 @app.post("/")
 async def api_endpoint(item: Item):
-
+    message = item.prompt + " " + item.data
     if item.service == 'openai':
         print("openai picked")
         client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": item.message}]
+            messages=[{"role": "user", "content": message}]
         )
-        print(f'response {response}')
         return {'response' : response.choices[0].message.content}
+
 
     elif item.service == 'ollama':
         print("ollama picked")
         response = ollama.chat(model='llama2', messages=[
             {
                 'role': 'user',
-                'content': item.message,
+                'content': message,
             },
         ])
-        print(f"response {response}")
         return {'response' : response['message']['content']}
+
 
     elif item.service == 'aws':
         print("aws picked")
         url = 'https://ldqnetwom0.execute-api.us-east-1.amazonaws.com/llm-test/llm'
-        params = {'question': item.message}
+        params = {'question': message}
         response = requests.get(url, params=params)
         return {'response' : response.json()[0]["generated_text"]}
     else:
